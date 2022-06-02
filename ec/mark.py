62afaa5906aa6c4b8a29485d15a9eefd1e1f2fd2
Angel Galvez Merchan <angelgalvez94@gmail.com>
2022-06-02 16:28:57 -0700

    Update mark.py

--- ec/mark.py
+++ ec/mark.py
@@ -16,19 +16,19 @@ def ec_mark(mp, mfc, gs, output_fname, deg_fname):
   dfc = df.query(f"{pval_col_name} <= {mp} & {fc_col_name} >= {mfc}")
   
   # mask out genes that are shared between max_gene_shares cell type
-  non_repeat_genes = dfc['name'].value_counts()[dfc['name'].value_counts() < gs].index.values
+  non_repeat_genes = dfc['target'].value_counts()[dfc['target'].value_counts() < gs].index.values
   
   # remove repeated genes, sort by effect size
-  m = dfc[dfc.name.isin(non_repeat_genes)].sort_values('es', ascending = False)
+  m = dfc[dfc.target.isin(non_repeat_genes)].sort_values(fc_col_name, ascending = False)
   
   # get number of genes per cell type to extract
-  n_sample = m["group_id"].value_counts().min()
+  n_sample = m["group"].value_counts().min()
   
   # extract markers with highet effect size
-  markers = m.groupby('group_id').tail(n_sample)
+  markers = m.groupby('group').tail(n_sample)
   
   # make dictionary
-  markers_dict = markers.groupby("group_id")["name"].apply(lambda x: list(x)).to_dict()
+  markers_dict = markers.groupby("group")["target"].apply(lambda x: list(x)).to_dict()
   
   # write dictionary
   write_markers(output_fname, markers_dict)
